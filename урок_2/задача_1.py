5c1ba2edfe2922393a6436de563dd23d40dfbd2e
SergeyLazarevich <sergeylazarevich@gmail.com>
2021-02-08 20:19:58 +0300

    удалил второй урок

--- урок_2/задача_1.py
+++
@@ -1,7 +0,0 @@
-list=[False,1,2.568,'hay',{1,2,2,4},{1:2,2:4},[1,5,"true"]]
-# index=0
-# while index<len(list):
-#     print(type(list[index]))
-#     index += 1
-for i in range(0,len(list)):
-    print(type(list[i]))
