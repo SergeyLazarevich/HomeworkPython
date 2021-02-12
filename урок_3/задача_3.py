e7917b7bca01690e0f9e6378f0796994d243a430
SergeyLazarevich <sergeylazarevich@gmail.com>
2021-02-12 21:02:08 +0300

    Улутшил код в задаче №3

--- урок_3/задача_3.py
+++ урок_3/задача_3.py
@@ -3,10 +3,9 @@ def max_sum(namber_1,namber_2,namber_3):
     функция принимает три позиционных аргумента и возвращает сумму наибольших двух аргументов
     """
     fun_list=[namber_1,namber_2,namber_3]
-    max_1=max(fun_list)
-    fun_list.remove(max_1)
-    max_2=max(fun_list)
-    return max_1+max_2
+    fun_min=min(fun_list)
+    fun_list.remove(fun_min)
+    return fun_list[0]+fun_list[1]
 
 my_namber_1=int(input("Введите первое число: "))
 my_namber_2=int(input("Введите второе число: "))
