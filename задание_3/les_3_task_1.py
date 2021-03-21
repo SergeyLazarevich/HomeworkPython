7cbb7e07162acff2a9e1e45d58293ff95ee3f247
SergeyLazarevich <sergeylazarevich@gmail.com>
2021-03-21 22:27:26 +0300

    lesson_3

--- задание_3/les_3_task_1.py
+++ задание_3/les_3_task_1.py
@@ -1,12 +1,10 @@
 """1. В диапазоне натуральных чисел от 2 до 99 определить, сколько из них кратны каждому из чисел в диапазоне от 2 до 9.
 Примечание: 8 разных ответов."""
 
-list_nuber = []
+list_nuber = [0]*8
 for i in range(2,100):
-    n = 0
     for j in range(2,10):
         if i % j == 0:
-            n +=1
-    if n >= 5:
-        list_nuber.append(i)
-print(f"Ответ: {list_nuber}")
+            list_nuber[j - 2] +=1
+for i, item in enumerate(list_nuber, start=2):
+    print(f"числу {i} кратно {item} чисел")
