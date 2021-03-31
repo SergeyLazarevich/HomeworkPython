08ee8fcf312cecbf3791ed1a7dc92d8f31d6fc48
SergeyLazarevich <sergeylazarevich@gmail.com>
2021-03-31 20:45:01 +0300

    lesson_8

--- задание_8/les_8_task_3.py
+++ задание_8/les_8_task_3.py
@@ -4,38 +4,33 @@
 a. граф должен храниться в виде списка смежности;
 b. генерация графа выполняется в отдельной функции, которая принимает на вход число вершин."""
 
-import collections
-
 def apvs(graph, start):
-    Y, X = set(), collections.deque([start])
+    Y = set() 
+    X = [start]
+    Z = []
     Y.add(start)
     while X:
-        start = X.popleft()
+        Z.append(X.pop(0))
         for next in graph[start]:
             if next not in Y:
                 Y.add(next)
                 X.append(next)
-        print(start)
+    return Z
 
 def graphs(num):
     if num < 2:
         print('Слишком мало вершин')
-        return
-    graph = []
-    for i in range(num):
-        for j in range(num):
-            if i != j:
-                graph.append((i, j))
-    list_graph = {}
-    for i in graph:
-        list_graph[str(i[0])] = [].append(str(i[1]))
-    return list_graph
+        return {'0':'0'}
+    graph = {i:{j for j in range(num) if i != j} for i in range(num)}
+    return graph
 
+n = int(input('Введите кличество вершин: '))
+number = int(input('С какой вершины вести счёт: '))
+print('Граф в виде списка смежности:')
+graph = graphs(n)
+#print(graph)
+for key, valume in graph.items():
+    print(f'{key} : {valume}')
+print('Путь в глубину:')
+print(apvs(graph,number))
 
-graph = {'0': set(['1', '2']),
-        '1': set(['0', '3', '4']),
-        '2': set(['0']),
-        '3': set(['1']),
-        '4': set(['2', '3'])}
-#apvs(graph,'0')
-graphs(int(input('Сколько друзей встретились: ')))
